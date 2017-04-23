4e6d1a2ca97532dfcff48335f532a3eb5491a982
feikool <feikool@sina.com>
2017-04-23 08:03:05 +0800

    Friend 组件更新

---
+++ src/Components/Common/edit.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Css/rlm_edit.css';
+class Edit extends Component{
+    render(){
+        return(
+            <div className="rlm-edit"></div>
+        )
+    }
+}
+export default Edit;
